9fcea303804edd4b8d332781c2c31b77a71733be
Tomos Griffiths <tomos.griffiths@dvla.gov.uk>
2024-03-06 13:48:27 +0000

    updated driver_id_validator tests

--- driver_id_validator_tests.rb
+++ driver_id_validator_tests.rb
@@ -2,43 +2,36 @@ require_relative 'driver_id_validator'
 require 'rspec/expectations'
 include RSpec::Matchers
 
-# a unit test method to check different vrns, and determine if they are valid or invalid
+# a unit test method to check different driver_ids, and determine if they are valid or invalid
 def validation_test(valid)
   if valid == true
-    return 'VRN valid'
+    return 'DriverID valid'
   else
-    return 'VRN invalid'
+    return 'DriverID invalid'
   end
 end
 
   tests = { #batch of inputs against what is expected of the validation
-    'BA72TGH' => 'VRN valid',
-    'CK61VBM' => 'VRN valid',
-    'AB55 CDE' => 'VRN valid',
-    'ABCDEFG' => 'VRN invalid',
-    '1111111' => 'VRN invalid',
-    'AA00AAA' => 'VRN valid',
-    'QA54BUN' => 'VRN valid',
-    'IG55XYZ' => 'VRN valid',
-    'JN34NLA' => 'VRN valid',
-    'TK54TOM' => 'VRN valid',
-    'UA55ATY' => 'VRN valid',
-    'ZW34NLA' => 'VRN valid',
-    '34AB567' => 'VRN invalid',
-    'AQ72PLV' => 'VRN valid',
-    'CI62JET' => 'VRN valid',
-    ',B54THN' => 'VRN invalid',
-    '?!45?!&' => 'VRN invalid',
-    'AB12345' => 'VRN invalid',
-    'AB55CDEF' => 'VRN invalid',
-    '""' => 'VRN invalid'
+    'SCHNL0792' => 'DriverID valid',
+    'WIZAJ0805' => 'DriverID valid',
+    'WARDX0482' => 'DriverID valid',
+    '2024-02-13' => 'DriverID invalid',
+    '' => 'DriverID invalid',
+    'SCHUL0567' => 'DriverID valid',
+    'ZIEMX0264' => 'DriverID valid',
+    'MCGLC0375' => 'DriverID valid',
+    'HOWE21083' => 'DriverID invalid',
+    '?!2345678' => 'DriverID invalid',
+    '2024C1152' => 'DriverID invalid',
+    'YOST21068' => 'DriverID invalid',
+    'MAYER504229MV4VN' => 'DriverID invalid',
+    'HERMC' => 'DriverID invalid',
+    '""' => 'DriverID invalid'
   }
 
   tests.each_pair do |key, value|
-    #trims the input if it has any whitespace within the string
-    trimmed_key = key.chomp.upcase.gsub(' ', '')
     #gives the result of the validation as a boolean
-    result = VrnValidator.validation(trimmed_key)
+    result = DriverIdValidator.validation(key)
     #performs the above unit test
     validated = validation_test(result)
     #output of the test compared to the given input
